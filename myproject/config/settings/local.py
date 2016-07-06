07ce399f2e077310e8a28746e4f4c4620604e4fd
LazerCube <git@lazercube.com>
2016-07-06 12:59:53 +0100

    Added MEDIA_ROOT and STATIC_ROOT

--- myproject/config/settings/local.py
+++ myproject/config/settings/local.py
@@ -3,5 +3,11 @@ from settings._base import *
 
 DEBUG = True
 
+STATIC_ROOT = (os.path.join(BASE_DIR, 'compiled_static'))
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
